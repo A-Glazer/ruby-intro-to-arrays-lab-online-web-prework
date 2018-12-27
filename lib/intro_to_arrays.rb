4fe9cb23c0d2f15b6cf165be66202eeb25591b1a
A Glazer <avigayilglazer@gmail.com>
2018-12-27 20:40:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,7 +6,7 @@ def array_with_two_elements
   return ["item1", "item2"]
 end
 
-def my_first_element(chart)
+def first_element(chart)
   chart = ["Welcome to New York", "item2"]
   return chart[0]
 end
