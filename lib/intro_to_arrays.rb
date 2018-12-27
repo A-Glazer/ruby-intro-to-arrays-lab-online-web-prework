c8376d4a0ccb7484a86d5b2b59bb2b2d7dae5333
A Glazer <avigayilglazer@gmail.com>
2018-12-27 20:49:18 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -27,6 +27,6 @@ def first_element_with_array_methods(chart)
 end
 
 def last_element_with_array_methods(chart)
-  chart = ["Thailand", "Welcome to New York", "item2", "Style", "Out of The Woods", "Southeast Asia"]
+  chart = ["Thailand", "Welcome to New York", "item2", "Style", "Out of The Woods", "Myanmar"]
   return chart[-1]
 end
